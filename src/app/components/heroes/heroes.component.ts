a5ccf13b2ac25e7943ca8e165d629c7c4e320233
Andrés Felipe Tapias Tuberquia <atapias@sistecredito.com>
2021-02-17 07:34:15 -0500

    Se añaden las rutas para cada componente y se aplican en el header, tambien se crea el servicio para obtener todos los heroes en un array de objetos, además se crean contenidos y estilos para algunas paginas, por ultimo se empieza a crear el estilo de las card de cada heroe.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html'
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
 
-  ngOnInit(): void {
+  constructor( private _heroesService: HeroesService ) { //Es el primero en ejecutarse
+
+  }
+
+  ngOnInit(): void { //Se ejecuta cuando la página ya esta renderizada
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
